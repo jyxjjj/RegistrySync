caabaaa632384638f1472926b0f7087b7f694ccb
jyxjjj <773933146@qq.com>
2025-11-12 14:30:27 +0800

    2025-11-12 14:30:27

--- deps.sh
+++ deps.sh
@@ -12,7 +12,7 @@ brew tap shivammathur/php >/dev/null 2>&1
 
 brew install skopeo shivammathur/php/php@8.4 >/dev/null 2>&1
 
-[ ! -f /usr/local/bin/composer ] && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer >/dev/null 2>&1
+[ ! -f /usr/local/bin/composer ] && curl -fSsL https://getcomposer.org/composer.phar -o /usr/local/bin/composer && chmod +x /usr/local/bin/composer
 
 echo "Installed versions:"
 echo -n "Skopeo: "
