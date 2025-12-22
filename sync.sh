19ddeb2b1904d78067c3fc7cbc4ca9574baa25fb
jyxjjj <773933146@qq.com>
2025-12-22 15:43:37 +0800

    2025-12-22 15:43:37

--- sync.sh
+++ sync.sh
@@ -4,21 +4,25 @@ set -e
 
 if [ -z "$REGISTRY_URL" ]; then
     echo "Error: REGISTRY_URL 环境变量未设置！"
+    echo "Example: 'REGISTRY_URL=registry.example.com', without http/https prefix."
     exit 1
 fi
 
 if [ -z "$MESSAGE_ENDPOINT_URL" ]; then
     echo "Error: MESSAGE_ENDPOINT_URL 环境变量未设置！"
+    echo "Example: 'MESSAGE_ENDPOINT_URL=https://example.com/api/message'."
     exit 1
 fi
 
 if [ -z "$MESSAGE_TOKEN_NAME" ]; then
     echo "Error: MESSAGE_TOKEN_NAME 环境变量未设置！"
+    echo "Example: 'MESSAGE_TOKEN_NAME=X-Token'."
     exit 1
 fi
 
 if [ -z "$MESSAGE_TOKEN" ]; then
     echo "Error: MESSAGE_TOKEN 环境变量未设置！"
+    echo "Example: 'MESSAGE_TOKEN=YOUR_TOKEN_VALUE'."
     exit 1
 fi
 
