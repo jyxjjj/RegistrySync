5a2b5999a97c22039a162b49a47051079d90ab92
jyxjjj <773933146@qq.com>
2026-05-13 10:46:31 +0800

    chore(default): 📝 Commit at 2026-05-13 10:46:31

--- sync.sh
+++ sync.sh
@@ -44,7 +44,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_DOWNLOAD_CONCURRENCY=8
 
 brew update
-brew tap shivammathur/php@8.4
+brew tap shivammathur/php
 
 brew install skopeo shivammathur/php/php@8.4
 
